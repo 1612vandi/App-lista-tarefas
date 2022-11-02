5b807636216167e56905b8784b07c3bd05525ae4
1612vandi <vandidanielmartinsfrancisco@gmail.com>
2022-11-02 20:37:30 +0000

    arquivos_classes

---
+++ conexao.php
@@ -0,0 +1,24 @@
+<?php
+
+class Conexao {
+
+  private $host ='localhost';
+  private $dbname ='bd_lista_tarefa';
+  private $user ='root';
+  private $pass ='';
+
+  public function conectar(){
+    try {
+      $conexao = new PDO(
+        "mysql:host=$this->host;dbname=$this->host",
+        "$this->user",
+        "$this->pass"
+      );
+      return $conexao;
+
+    } catch (PDOException $e) {
+      echo '<p>'.$e->getMessege().'/<p>';
+    }
+  }
+}
+?>
---
+++ tarefa.service.php
@@ -0,0 +1,21 @@
+<?php
+
+// crud
+class tarefaService{
+
+  public function inserir(){ // creat
+
+  }
+
+  public function recuperar(){ // read
+
+  }
+
+  public function actualizar(){ // update
+
+  }
+
+  public function remover(){ // delete
+
+  }
+}
